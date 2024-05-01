3a291164758fa3e1e2740e6f12b794bdc977fafe
ebu3565 <mdebrahim3565@gmail.com>
2024-05-02 00:52:59 +0600

    null vs undefined check

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+let pakhi;
+function add(num1, num2){
+  console.log(num1 + num2);
+}
+
+
+const result = add(13);
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,7];
+console.log(ages[11]);
