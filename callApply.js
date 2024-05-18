2962f5c51acfcd009e0e22836674d7544c247b90
ebu3565 <mdebrahim3565@gmail.com>
2024-05-19 01:46:10 +0600

    bind

--- callApply.js
+++ callApply.js
@@ -6,11 +6,34 @@ const normalPerson = {
     console.log(this.firstName, this.lastname);
   },
   chargeBill: function(amount){
+    console.log(this);
     this.salary = this.salary - amount;
     return this.salary;
   }
 }
-normalPerson.chargeBill(1500);
-normalPerson.chargeBill(20);
-console.log(normalPerson.salary);
+// normalPerson.chargeBill(1500);
+// normalPerson.chargeBill(20);
+// console.log(normalPerson.salary);
 // console.log(normalPerson.firstName);
+
+const heroPerson = {
+  firstName: 'balam',
+  lastname: 'Uddin',
+  salary: 15000
+}
+
+const friendlyPerson = {
+  firstName: 'Golam',
+  lastname: 'Uddin',
+  salary: 25000
+}
+// normalPerson.chargeBill();
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(2000);
+heroChargeBill(3000);
+
+const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyChargeBill(1000);
+friendlyChargeBill(1000);
+
+console.log(friendlyPerson.salary);
