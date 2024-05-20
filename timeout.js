30ea218dadcfe9bcd9139660dc4ddbd4724a5fda
ebu3565 <mdebrahim3565@gmail.com>
2024-05-21 02:42:16 +0600

    timeout and setInterval

---
+++ timeout.js
@@ -0,0 +1,11 @@
+function doSomething(){
+  console.log(3333);
+} 
+console.log(4444);
+setInterval(()=>{
+  console.log('see you later mate')
+}, 1000);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
